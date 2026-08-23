6cca4e6b40a8f848b401891fa35394778320d8fc
Koh Yong De <kohyongde@gmail.com>
2026-08-23 13:53:03 +0800

    Doug can add things to list

--- src/main/java/DougMethods.java
+++ src/main/java/DougMethods.java
@@ -1,10 +1,29 @@
 public class DougMethods {
+    private static String[] list = new String[100];
+    private static int listSize = 0;
+
     private static void echo(String input) {
         System.out.printf("\t %s\n", input);
         DougLines.printLine();
     }
 
+    private static void getList() {
+        for (int i = 0; i < listSize; ++i) {
+            System.out.printf("\t%d. %s\n", i + 1, list[i]);
+        }
+        DougLines.printLine();
+    }
+
+    private static void addToList(String input) {
+        list[listSize++] = input;
+        System.out.printf("\tadded: %s\n", input);
+        DougLines.printLine();
+    }
+
     public static void reply(String input) {
-        echo(input);
+        switch (input.toLowerCase()) {
+            case "list" -> getList();
+            default -> addToList(input);
+        }
     }
 }
